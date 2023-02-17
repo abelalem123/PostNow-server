c7ca430abb934317b6ba99491dba11ec06cf6f14
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-02-17 13:54:35 +0300

    back end

---
+++ controllers/auth.js
@@ -0,0 +1,57 @@
+import bcrypt, { hash } from "bcrypt"
+import User from "../models/user.js"
+import jwt from "jsonwebtoken"
+// Register user
+export const register=async(req,res)=>{
+     try {
+        const{
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation
+        }=req.body
+       // const salt=bcrypt.genSalt()
+        const passwordHash=await bcrypt.hash(password,10)
+        const newUser=new User({
+            firstName,
+            lastName,
+            email,
+            password:passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile:Math.floor(Math.random()*1000),
+            impressions:Math.floor(Math.random()*1000)
+        })
+        const savedUser=await newUser.save()
+        res.status(201).json(savedUser)
+     } catch (error) {
+        res.status(500).json({error:error.message})
+     }
+}
+
+// Login
+export const login=async(req,res)=>{
+try {
+    const {email,password}=req.body
+    const user=await User.findOne({email:email})
+    if(!user) return res.status(400).json({msg:"user doesnot exist"})
+    
+    const isMatch=await bcrypt.compare(password,user.password)
+
+    if(!isMatch) return res.status(400).json({msg:"invalid credentials"})
+    
+    const token=jwt.sign({id:user._id},process.env.JWT_SECRET);
+    // console.log(token);
+    delete user.password
+    res.status(200).json({token,user})
+
+} catch (error) {
+    res.status(500).json({error:error.message})
+}
+}
